b9399cc6ff2c102237f63c050a1421ffc781685b
Harold Castro <hcastro@enchufate.pe>
2020-03-21 17:51:54 -0500

    appcenter configuration

--- app/src/main/java/pe/harold/testappcenter/MainActivity.java
+++ app/src/main/java/pe/harold/testappcenter/MainActivity.java
@@ -1,6 +1,9 @@
 package pe.harold.testappcenter;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.microsoft.appcenter.AppCenter;
+import com.microsoft.appcenter.analytics.Analytics;
+import com.microsoft.appcenter.crashes.Crashes;
 
 import android.os.Bundle;
 
